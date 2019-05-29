a80d2c73d7b7257e98d872d014cf69fc68176f9b
Marten Schälicke <hallo@emm-ess.de>
2019-05-29 19:19:27 +0200

    textupdate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,9 +18,8 @@
         </div>
         <div v-else>
             <h1>Programm des IJT 2019</h1>
-            <p>Eines direkt als erstes: diese Seite ist in keinster Weise offiziell und steht nicht in Verbindung mit der NAK oder der IJT 2019 Düsseldorf gGmbH. Daher sind Reservierungen oder andere mit einem Login auf ijt2019.org verbundene Aktionen nicht möglich.</p>
-            <p>Dies ist ein Versuch, in etwas über 48 Stunden und mit zu wenig Schlaf, eine digitale & offline-fähige Version der Programm-Übersicht zu schaffen. Bis zum IJT sind noch weitere Funktionen, beispielsweise ein Bookmarking-System, geplant. Eine englische Version ist zwar vorbereitet, gestaltet sich auf Grund der Datenlage jedoch als schwierig.</p>
-            <p>Sobald du Veranstaltungen als "gemerkt" markiert hast, werden sie hier erscheinen. Mehr Informationen findest du auf der <router-link to="/about">Seite über dieses Projekt</router-link>.</p>
+            <p><strong>Eines direkt als erstes: diese Seite ist weder offiziell, noch stammt sie von der NAK oder der IJT 2019 Düsseldorf gGmbH. Daher sind Reservierungen oder andere mit einem Login auf ijt2019.org verbundene Aktionen hier nicht möglich.</strong></p>
+            <p>Um es kurz zu machen, diese WebApp ist dein digitaler Begleiter auf dem Jugendtag. Du kannst durch das Programm stöbern und deinen eigenen Plan erstellen, den du dann genau hier siehst. Mehr Informationen findest du auf der <router-link to="/about">Projekt-Seite</router-link>.</p>
         </div>
     </div>
 </template>
